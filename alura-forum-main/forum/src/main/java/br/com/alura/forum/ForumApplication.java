03a52a5d65fbc4dfcb952b318bdf25ec47304543
Eleazar89 <122639455+Eleazar89@users.noreply.github.com>
2023-05-16 15:35:52 -0600

    Add files via upload

---
+++ alura-forum-main/forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+//Com essa anotação (@EnableSpringDataWebSupport) habilitamos esse suporte, para o Spring pegar da requisição, dos parâmetros da url os campos, 
+//as informações de paginação e ordenação, e repassar isso para o Spring data.
+@EnableSpringDataWebSupport 
+@SpringBootApplication
+//habilita o uso de cache na aplicação.
+@EnableCaching
+@EnableSwagger2
+public class ForumApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ForumApplication.class);
+	}
+
+}
